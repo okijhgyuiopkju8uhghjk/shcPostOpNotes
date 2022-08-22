f2d356eca0146f860b86dc68d3f74267c1033f9d
okijhgyuiopkju8uhghjk <994984@gmail.com>
2022-08-22 12:07:37 +0530

    one more commit

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function blinkText(){
-    var f = document.getElementById("testElement");
-    setInterval(function(){
-        f.style.visibility = (f.style.visibility == 'hidden'?'':'hidden');
-    },1000);
+//first wait for the DOM to load
+window.onload = function(){
+    //get date from dom
+    //get sur end time from dom
+    //import js date time script
+    //get the value of sur end time..
+    document.getElementById("surEndTime").onclick= 
+    function(){alert(document.getElementById("surEndTimeValue").value +document.getElementById("currentDate").value );}
+    
+
 }
