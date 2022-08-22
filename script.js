45961f0400cec3f91b8a959c6aedbce46d30397c
okijhgyuiopkju8uhghjk <994984@gmail.com>
2022-08-22 12:56:25 +0530

    module added

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+import addHours from './addTime.mjs'
 //first wait for the DOM to load
 window.onload = function(){
     //get date from dom
     //get sur end time from dom
+    document.getElementById("surEndTime").onclick= 
+    function(){
+        myDate = new Date("2022-03-14T"+document.getElementById("surEndTimeValue").value);
+        //alert(document.getElementById("surEndTimeValue").value +document.getElementById("currentDate").value );
+        document.getElementById("liquidDiteAt").innerHTML=(addHours(2, mydate)).toLocaleString('en-US', {
+            //day:    'numeric',
+            //month:  'numeric',
+            //year:   'numeric',
+            hour:   '2-digit',
+            minute: '2-digit',
+            hours12: "true"
+            
+        });
+    }
+    
     //import js date time script
     //get the value of sur end time..
-    document.getElementById("surEndTime").onclick= 
-    function(){alert(document.getElementById("surEndTimeValue").value +document.getElementById("currentDate").value );}
     
 
 }
